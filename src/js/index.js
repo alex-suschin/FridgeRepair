5d092b98ca26b043404a3b53732c59bd979ab958
alex-suschin <alex_suschin@mail.ru>
2020-07-09 17:26:20 +0300

    question

--- src/js/index.js
+++ src/js/index.js
@@ -23,6 +23,29 @@ $(function() {
         return false;
     });
 
+    $('.btn-dashed-js').click(function() {
+        $(this).toggleClass('active');
+        if ($(this).hasClass('active')) {
+            $('.rewiews-box__items').addClass('active');
+            $(this).text('Скрыть');
+        } else {
+            $(this).removeClass('active');
+            $('.rewiews-box__items').removeClass('active');
+            $(this).text('Показать все отзывы');
+        }
+    });
+
+    $('.questions-item-js').click(function() {
+        $(this).toggleClass('active');
+        if ($(this).hasClass('active')) {
+            $(this).children('.questions-item--answear').show(200);
+        } else {
+            $(this).removeClass('active');
+            $(this).children('.questions-item--answear').hide(200);
+        }
+
+    });
+
     $(window).on('load resize', function() {
 
         var width = $(window).width();
